cb09b5f8a61e74858ce6c5e645bca1d62b61ba0d
rdalmau94 <80904933+rdalmau94@users.noreply.github.com>
2021-03-20 10:36:43 +0100

    He canviat el titol del projecte.

--- app/src/main/java/com/example/cursprovaudemy/MainActivity.java
+++ app/src/main/java/com/example/cursprovaudemy/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.cursprovaudemy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textPrin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textPrin = findViewById(R.id.textPr);
+        textPrin.setText("CANVI PROJECTE");
     }
 }
