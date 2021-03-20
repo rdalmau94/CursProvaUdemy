b06131d3228050b10f1339533d2256d5f4d7e299
Ricard <ricarddalmaucasals@gmail.com>
2021-03-20 19:48:05 +0100

    Canvi text pantalla des de Lenovo.

--- app/src/main/java/com/example/cursprovaudemy/MainActivity.java
+++ app/src/main/java/com/example/cursprovaudemy/MainActivity.java
@@ -15,6 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textPrin = findViewById(R.id.textPr);
-        textPrin.setText("CANVI PROJECTE");
+        textPrin.setText("CANVI PROJECTE DESDE LENOVO");
     }
 }
